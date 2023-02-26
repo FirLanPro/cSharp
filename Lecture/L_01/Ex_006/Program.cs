97e87b04105c1ad9b4b0d42719b19d19d318ddfc
FirLanPro <FirLanPro@yandex.ru>
2023-02-26 06:55:10 +0300

    L01 задача_006

--- Lecture/L_01/Ex_006/Program.cs
+++ Lecture/L_01/Ex_006/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "имя")
+{
+    Console.WriteLine("Ура Имя");
+}
+else
+{
+    Console.Write("текст, ");
+    Console.WriteLine(username);
+}
