34683e8382134c7ea65b75828fadd97f9f35e411
FirLanPro <FirLanPro@yandex.ru>
2023-02-26 14:39:35 +0300

    home dz_001 Задача_3

---
+++ Homework/DZ_001/zadacha_003/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a%2==0)
+{
+    Console.Write("четное число ");
+    Console.WriteLine(a);
+}
+else 
+{
+   Console.Write("нечетное число ");
+    Console.WriteLine(a); 
+}
+
