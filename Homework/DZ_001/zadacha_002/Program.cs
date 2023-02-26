f13d0bcb6994996918e027a2bfac8bc61067489a
FirLanPro <FirLanPro@yandex.ru>
2023-02-26 14:25:06 +0300

    home dz_001 Задача_2

--- Homework/DZ_001/zadacha_002/Program.cs
+++ Homework/DZ_001/zadacha_002/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int c= Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b>max) max = b;
+if (c>max) max = c;
+
+
+Console.Write("МАКСИМАЛЬНОЕ= ");
+Console.WriteLine(max);
+
+
