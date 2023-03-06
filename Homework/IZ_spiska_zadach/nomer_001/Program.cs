0f63315ef226e0a30abf66898ced7cc2a11e8ad3
FirLanPro <FirLanPro@yandex.ru>
2023-03-06 10:45:46 +0300

    IZ_spiska nomer_001

---
+++ Homework/IZ_spiska_zadach/nomer_001/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход Слово, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write ("Введите день недели :  ");
+string WEEK = Console.ReadLine();
+
+string NN (string WEEK)
+{
+if (WEEK.ToLower() == "суббота" || WEEK.ToLower() == "воскресение")
+    {
+    return "выходной день";
+    }
+return "таких выходных дней нет";
+}
+
+string NM (string WEEK)
+{
+    if (WEEK.ToLower() == "понедельник"|| WEEK.ToLower() =="вторник"|| WEEK.ToLower() =="среда"|| WEEK.ToLower() =="четверг"|| WEEK.ToLower() =="пятница")
+    {
+    return "будний день";
+    }
+return "таких будних дней нет";
+}
+NN (WEEK);
+string res = NN (WEEK);
+Console.WriteLine(res);
+NM ( WEEK);
+string rez = NM (WEEK);
+Console.WriteLine(rez);
