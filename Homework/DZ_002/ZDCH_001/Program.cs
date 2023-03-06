ba200c60d03652ea6c613dacabd3c682ca15aad9
FirLanPro <FirLanPro@yandex.ru>
2023-03-06 06:36:55 +0300

    DZ_002 ZDCH_001

--- Homework/DZ_002/ZDCH_001/Program.cs
+++ Homework/DZ_002/ZDCH_001/Program.cs
@@ -1,37 +1,29 @@
-﻿int Number ( int N)
-{
-int e=0;
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-if  (N /100 != 0 && N /1000 == 0)
+int Number ( int N)
 {
-    e = N / 10%10;
- Console.Write("вторая цифра в числе: ");
-}   
-  else
+   int e=0;
+   if  (N /100 != 0 && N /1000 == 0)
    {
-    e=N;
-  Console.Write("не 3 цифры в числе: ");
-    
-     }
- 
- return e;
+      e = N / 10%10;
+      Console.Write("вторая цифра в числе: ");
+      
+   }   
+return e;
 }
 
-Console.Write("Введите 3-х значное число: ");
+void Num ( int N )
+{
+  if (N /100 == 0 || N /1000 != 0)
+  {
+     Console.WriteLine ("не 3 цифры ");
+   }
+  return ;
+}
+
+Console.WriteLine("Введите 3-х значное число: ");
 int N = Convert.ToInt32(Console.ReadLine());
+Num (N);
+
 int res= Number (N);
 System.Console.WriteLine(res);
-
-
-
-
-
-//if (N /100 != 0 && N /1000 == 0)
-//{
- //   e = N / 10%10;
- //   Console.WriteLine(e);
-//}   
- //  else
-  // {
- //  Console.WriteLine("не 3 знака");
-  // }
