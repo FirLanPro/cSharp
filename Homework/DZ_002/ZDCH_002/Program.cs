83a60beb813ae7e62a909628690d2e29ea66f86a
FirLanPro <FirLanPro@yandex.ru>
2023-03-06 09:04:03 +0300

    DZ_002 ZDCH_002

---
+++ Homework/DZ_002/ZDCH_002/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. (3-я цифра с начала числа).
+
+int e=0;
+int a=0;
+int Bnum (int N){
+    if  (N /1000 != 0)
+  {
+  
+    while (N > 999)
+     {
+    N= N/ 10;
+      }
+     a=N%10;
+    Console.Write("третья цифра числа: ");
+  }
+return a;
+}
+
+int Number ( int N)
+{
+   if  (N /100 != 0 && N /1000 == 0)
+   {
+      e = N % 10;
+      Console.Write("третья цифра числа: ");
+   }
+
+    return e;
+}
+
+void Num ( int N )
+{
+  if (N /100 == 0)
+  {
+     Console.WriteLine ("3-ей цифры нет ");
+   }
+  return ;
+}
+
+Console.WriteLine("Введите  число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Num (N);
+
+int Bnu =Bnum(N);
+System.Console.WriteLine(Bnu);
+
+
+int res = Number (N);
+System.Console.WriteLine(res);
