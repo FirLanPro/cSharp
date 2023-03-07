09936e09405274f6edc4786d3327a3bf44cb33d1
FirLanPro <FirLanPro@yandex.ru>
2023-03-07 16:21:34 +0300

    s_003 ZD_03

---
+++ Seminar/S_003/ZD_03/Program.cs
@@ -0,0 +1,17 @@
+﻿// Кубы чисел от 1 до N. вариант 2 
+string Numbers (int N)
+{
+   string strok =String.Empty;
+ int a = 1;
+ while (a <= N)
+    {
+        int result=a*a*a;
+        strok=strok+result + " ";
+        a = a + 1;
+    }
+    return strok;
+}
+    Console.Write("Введите число: ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    string vivod = Numbers (N);
+    System.Console.WriteLine(vivod);
