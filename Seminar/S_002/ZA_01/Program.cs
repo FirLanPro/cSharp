18b7b4ba22af85dffec32d151da81575608ae359
FirLanPro <FirLanPro@yandex.ru>
2023-03-07 15:44:12 +0300

    s_002 ZA_01

---
+++ Seminar/S_002/ZA_01/Program.cs
@@ -0,0 +1,27 @@
+﻿// принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23. вариант 1
+
+14 -> нет 46 -> нет 161 -> да
+
+int GetNumber(string text)
+{
+  Console.Write(text);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+bool Check(int value)
+{
+  return value % 7 == 0 && value % 23 == 0;
+}
+
+string GetResult(int number, bool checkResult)
+{
+  string temp = String.Empty;
+  if (checkResult) { temp = "YES"; }
+  else { temp = "NO"; }
+  return "Число: " + number + ". Итог: " + temp;
+}
+
+int value = GetNumber("Введите value: ");
+bool flag = Check(value);
+string output = GetResult(value, flag);
+System.Console.WriteLine(output);
