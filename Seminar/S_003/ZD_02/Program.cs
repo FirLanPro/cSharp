4589ec92056f8cf0179db89c96e122052339ffc1
FirLanPro <FirLanPro@yandex.ru>
2023-03-07 16:17:23 +0300

    s_003 ZD_02

---
+++ Seminar/S_003/ZD_02/Program.cs
@@ -0,0 +1,23 @@
+﻿// Кубы чисел от 1 до N
+int GetNumber(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+string GetResult(int n)
+{
+    string result = String.Empty;
+    int index =1;
+    while (index<=n)
+    {
+        int sqr = index*index*index;
+        result =result + "[" + index + "=>" + sqr + "]";
+        index = index +1;
+    }
+    return result;
+}
+int n=GetNumber("введите кончное число: ");
+Console.Write(GetResult(n));
+
+
+
