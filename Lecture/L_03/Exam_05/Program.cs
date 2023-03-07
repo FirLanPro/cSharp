3d69aec9fecb8977582150ad4dbaae15e06dc5e8
FirLanPro <FirLanPro@yandex.ru>
2023-03-07 14:16:06 +0300

    L_03 Exam_05

---
+++ Lecture/L_03/Exam_05/Program.cs
@@ -0,0 +1,14 @@
+﻿// Виды методов: 4. возвращ и приним
+string Method4(int count, string c)
+{
+    int i=0;
+    string result=""; // String.Empty;
+    while (i<count)
+    {
+        result = result + c;
+        i++;
+    }
+    return result;
+}
+string res = Method4 (10, "asnf ");
+Console.WriteLine(res);
