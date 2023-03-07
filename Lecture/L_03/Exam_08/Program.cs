06b0df2fe5ffc74e8788ea107f1ab38643a2bd39
FirLanPro <FirLanPro@yandex.ru>
2023-03-07 14:55:47 +0300

    L_03 Exam_08

---
+++ Lecture/L_03/Exam_08/Program.cs
@@ -0,0 +1,20 @@
+﻿// замена символов в тексте
+string text = "аолрфралораоаовлаоа";    ///аргумент не удается преобразовать
+          
+string Replace( string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length =text.Length;
+    for (int i=0; i< length; i++)
+    {
+        if (text[i]=oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    
+    }
+    return result;
+}
+string newText = Replace(text, " ", "|");
+Console.WriteLine(newText);
+Console.WriteLine();
+newText= Replace(newText, "о", "HH");
+Console.WriteLine(newText);
