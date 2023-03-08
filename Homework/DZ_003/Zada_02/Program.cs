ddab178c5f25d1d28c0a9308c9afd225db42e006
FirLanPro <FirLanPro@yandex.ru>
2023-03-08 14:02:22 +0300

    DZ_003 Zada_02

---
+++ Homework/DZ_003/Zada_02/Program.cs
@@ -0,0 +1,17 @@
+﻿// метод(-ы), который принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// корень из x2-x1 в квадрате + y2-y1 в квадрате + z2-z1 в квадрате
+
+int x1 = 4, y1 = 4, z1 = 3,
+    x2 = 5, y2 = 7, z2 = 6;
+
+double formula (int x1, int x2, int y1,int y2, int z1,int z2)
+{
+int a=x2-x1;
+int b=y2-y1;
+int c=z2-z1;
+double d=Math.Sqrt(a+b+c);
+return d;
+}
+formula(x1,x2,y1,y2,z1,z2);
+double res = formula(x1,x2,y1,y2,z1,z2);
+Console.WriteLine ($"расстояние= {res}");
