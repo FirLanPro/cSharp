c1145217d3d61327d41ece87cf79276f20f5f183
FirLanPro <FirLanPro@yandex.ru>
2023-03-09 05:34:30 +0300

    DZ_003 Zadz_01 редактирование

--- Homework/DZ_003/Zada_01/Program.cs
+++ Homework/DZ_003/Zada_01/Program.cs
@@ -1,56 +1,43 @@
 ﻿// Задача 19 Напишите метод(-ы), который принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-Console.Write ("введите пятизначное число : ");
-int N = Convert.ToInt32(Console.ReadLine());
-
-
 string NN (int N)
 {
 if (N<99999 && N>9999)
     {
-    return "5 цифр";
+    Console.WriteLine ();
     }
-    Console.WriteLine ("не 5 цифр");
-return "hj";
+    else
+    {
+    Console.WriteLine ("не 5 цифр. перезапустите, введите пятизначное число");
+    }
+return "количество цифр";
 }
 
 int Polindrom (int N)
-{int m=N%10;
-        int x=N/10000;
-if (N<99999 && N>9999) 
-{      
-    if (m!=x && N>999)
-        {
-        Console.WriteLine("не полиндром");
-        
-        }
-    else {
-        int y=N/10000;
-        N=N-y*10000;
+{
+    if (N<99999 && N>9999){
+    int m=N%10;
+    int x=N/10000;
+   
+    if (m==x && N>9999 ) { 
+        N=N%10000;
         N=N/10;
-        x=N/100;
         m=N%10;
-        Console.WriteLine(N);
-    }
-    if (m!=x && N<99)
-        {
-        Console.WriteLine("не полиндром");
+        x=N/100;
         }
+             
+    if (m==x && N>99)  {
+         N=N%100;
+         N=N/10;
+         Console.WriteLine("палиндром ");
+        } 
     else {
-        int y=N/100;
-        N=N-y*100;
-        N=N/10;
-        
-        Console.WriteLine(N);
-
-    }
-
-}
+        Console.WriteLine("не палиндром") ; 
+         } 
+     }
 return N;
 }
-Polindrom(N);
+Console.Write ("введите пятизначное число : ");
+int N = Convert.ToInt32(Console.ReadLine());
 NN (N);
-
-
-
-        
+Polindrom( N);        
