d5d8ba26f6d8d5b26c61226b6e01693231560102
FirLanPro <FirLanPro@yandex.ru>
2023-03-09 12:55:40 +0300

    DZ_003 Zada_02 редактирование

--- Homework/DZ_003/Zada_02/Program.cs
+++ Homework/DZ_003/Zada_02/Program.cs
@@ -6,12 +6,13 @@ int x1 = 4, y1 = 4, z1 = 3,
 
 double formula (int x1, int x2, int y1,int y2, int z1,int z2)
 {
-int a=x2-x1;
-int b=y2-y1;
-int c=z2-z1;
-double d = Math.Sqrt(a+b+c);
+    int a=x2-x1;
+    int b=y2-y1;
+    int c=z2-z1;
+    double d = Math.Sqrt(a+b+c);
 return d;
 }
+
 formula(x1,x2,y1,y2,z1,z2);
 double res = formula(x1,x2,y1,y2,z1,z2);
 Console.WriteLine ($"расстояние= {res}");
