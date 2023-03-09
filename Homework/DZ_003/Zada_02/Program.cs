6f65098ef6961434d869113e74a61be4609c35fa
FirLanPro <FirLanPro@yandex.ru>
2023-03-09 05:44:55 +0300

    DZ_003 Zadz_02 редактирование

--- Homework/DZ_003/Zada_02/Program.cs
+++ Homework/DZ_003/Zada_02/Program.cs
@@ -9,7 +9,7 @@ double formula (int x1, int x2, int y1,int y2, int z1,int z2)
 int a=x2-x1;
 int b=y2-y1;
 int c=z2-z1;
-double d=Math.Sqrt(a+b+c);
+double d = Math.Sqrt(a+b+c);
 return d;
 }
 formula(x1,x2,y1,y2,z1,z2);
