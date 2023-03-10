557decef9446092f66af0639aefede735615e5b3
FirLanPro <FirLanPro@yandex.ru>
2023-03-10 10:36:13 +0300

    DZ_004 Zz_02

---
+++ Homework/DZ_004/Zz_02/Program.cs
@@ -0,0 +1,21 @@
+﻿// программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int input (string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int round (int N)
+{
+    int X=0;
+    while  ( N > 0 )
+    {
+        X=X+N%10;
+        N=N/10;
+    }
+    return X;
+}
+int N = input("Введите число N: ");
+int res = round (N);
+Console.WriteLine($"сумма цифр в числе {N} = {res}");
+
+
