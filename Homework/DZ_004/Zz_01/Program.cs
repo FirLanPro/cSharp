a239db47217f542894ef97373a4278feea29b4a9
FirLanPro <FirLanPro@yandex.ru>
2023-03-10 13:22:24 +0300

    DZ_004 Zz_01

--- Homework/DZ_004/Zz_01/Program.cs
+++ Homework/DZ_004/Zz_01/Program.cs
@@ -1,4 +1,9 @@
 ﻿//  цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// основание -A с четной степенью = A с темже значением степени.
+// основание -A с нечетной степенью = -A с темже значением степени.
+// основание A с отрицательной степенью = 1/A  темже значением степени.
+// основание -A с отрицательной четной степенью = 1/A  темже значением степени.
+// основание -A с отрицательной нечетной степенью = 1/-A  темже значением степени.
 int vvod (string text)
 {
     Console.Write(text);
@@ -7,13 +12,30 @@ int vvod (string text)
 int round (int A, int B)
 {
     int C=1;
+    if (A!=0)
+    {
+    B=Math.Abs(B);
     for (int i=0;i<B; i++ )
     {
         C=C*A;
     }
+    }
+    else {Console.Write($"A не может быть =0"); }
     return C;
+}
+string negative (int B)
+{
+    if (B<0)
+    {
+        Console.Write($"1/"); 
+    }
+     
+    return "";
 }
 int A = vvod("Введите число A: ");
 int B = vvod("Введите число B: ");
 int res = round (A,B);
-Console.WriteLine ($"Число {A} в степени {B} = {res}");
+Console.Write ($"Число {A} в степени {B} = ");
+string res1 = negative (B);
+Console.Write ($"{res1}");
+Console.WriteLine($"{res}");
