bb69e5ec01d6b165576a9e0a0ce9bb5f049aa2e9
FirLanPro <FirLanPro@yandex.ru>
2023-03-11 15:11:17 +0300

    DZ_005 Za_02

---
+++ Homework/DZ_005/Za_02/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+
+void ArrayRandom (int [] NumberArr)
+{
+    int size=NumberArr.Length;
+    for (int i=0; i<size; i++)
+    {
+        NumberArr [i]= new Random().Next(1,20);
+        Console.Write(NumberArr[i] +" ");
+    }
+}
+
+int [] array =new int [8];
+ArrayRandom (array);
+
+int EvenNumb (int [] NumberArr)
+{
+    int n=0;
+    int size=NumberArr.Length;
+    for (int i=0; i<size; i++)
+    {
+        if ( i%2!=0)
+        {
+            n=n+NumberArr[i];
+         }
+    }
+    return n;
+}
+EvenNumb (array);
+Console.WriteLine($"сумма элементов на нечётных позициях = "+ EvenNumb (array));
