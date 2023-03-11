83e483653d9c351142d5b431ae8c21ab0eb78911
FirLanPro <FirLanPro@yandex.ru>
2023-03-11 14:56:05 +0300

    DZ_005 Za_01

--- Homework/DZ_005/Za_01/Program.cs
+++ Homework/DZ_005/Za_01/Program.cs
@@ -1,1 +1,28 @@
 ﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+void ArrayRandom (int [] NumberArr)
+{
+    int size=NumberArr.Length;
+    for (int i=0; i<size; i++)
+    {
+        NumberArr [i]= new Random().Next(100,999);
+        Console.Write(NumberArr[i] +" ");
+    }
+}
+int [] array =new int [8];
+ArrayRandom (array);
+
+int EvenNumb (int [] NumberArr)
+{int n=0;
+ int size=NumberArr.Length;
+for (int i=0; i<size; i++){
+   if ( NumberArr[i]%2==0)
+   {
+    n=n+1;
+   }
+}
+return n;
+}
+EvenNumb (array);
+Console.WriteLine($"четных чисел:"+ EvenNumb (array));
