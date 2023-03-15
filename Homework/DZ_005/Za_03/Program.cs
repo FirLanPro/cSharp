57f9897a106ce4d9fbfe781f53e27c4a9025fee4
FirLanPro <FirLanPro@yandex.ru>
2023-03-15 07:44:30 +0300

    DZ_005 Za_03

--- Homework/DZ_005/Za_03/Program.cs
+++ Homework/DZ_005/Za_03/Program.cs
@@ -1,2 +1,43 @@
 ﻿// массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива
+int input (string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void ArrayRandom (int [] NumberArr)
+{
+    int size=NumberArr.Length;
+    for (int i=0; i<size; i++)
+    {
+        NumberArr [i]= new Random().Next(1,50);
+        Console.Write(NumberArr[i] +", ");
+    }
+}
 
+int MaxMin (int [] NumberArr)
+{
+    int max=0;
+    int min = NumberArr[0];
+     int size=NumberArr.Length;
+    for (int i=0; i<size; i++)
+    {
+        if (NumberArr[i]>max)
+        {
+            max=NumberArr[i];
+        }
+        if (NumberArr[i]<min)
+        {
+           min=NumberArr[i];
+        }
+    }
+    return max-min;
+    
+   
+}
+int A = input("введите количество элементов в массиве: ");
+int [] array =new int [A];
+ArrayRandom (array);
+Console.WriteLine();
+int res = MaxMin (array);
+Console.WriteLine("разница между максимальным и минамальным элементом массива:  "+ res);
+//int B = input("Введите число B: ");
