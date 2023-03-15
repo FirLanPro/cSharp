0bab2e4db489ef6684ce73fba0b5678c7d9566e6
FirLanPro <FirLanPro@yandex.ru>
2023-03-15 14:53:56 +0300

    S_006 Z_01

---
+++ Seminar/S_006/Z_01/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте массивю Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+bool Check (int [] array, int el)
+{
+    int size = array.Length;
+    for (int i = 0; i < size; i++ )
+        if (array[i] ==el ) return true;
+    return false;
+}
+
+string PrintResult (int[] array, int el, bool exist)
+{
+    if(exist) return $"В массиве {String.Join(" ",array)} элемент {el} присутствует";
+    return "В массиве элемента нет";
+}
+int [] arr = {1,2,3,4,5,6,7,8,9};
+int find = 2;
+bool c = Check(arr,find);
+System.Console.WriteLine(PrintResult(arr,find,c));
