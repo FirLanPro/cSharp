e79ed58616723175210ae825fd10ef615a1f4a59
FirLanPro <FirLanPro@yandex.ru>
2023-03-15 10:21:33 +0300

    DZ_005 Za_02

--- Homework/DZ_005/Za_02/Program.cs
+++ Homework/DZ_005/Za_02/Program.cs
@@ -1,31 +1,39 @@
-﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях (нечетный индекс элемента).
 
-
-void ArrayRandom (int [] NumberArr)
+int Input (string text)
 {
-    int size=NumberArr.Length;
-    for (int i=0; i<size; i++)
-    {
-        NumberArr [i]= new Random().Next(1,20);
-        Console.Write(NumberArr[i] +" ");
-    }
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
 }
 
-int EvenNumb (int [] NumberArr)
+string ArrayRandom (int [] array)
 {
-    int n=0;
-    int size=NumberArr.Length;
+    int size =array.Length;
+    string resultArr = String.Empty;
     for (int i=0; i<size; i++)
     {
-        if ( i%2!=0)
+        array [i]= new Random().Next(-1000,1000);
+        resultArr = resultArr + array[i] + " ";
+    }
+    return resultArr;
+}
+
+int EvenNumb (int [] arrayString)
+{
+    int oddNumber = 0;
+    int size = arrayString.Length;
+    for (int i=0; i < size; i++)
+    {
+        if ( i % 2 !=0)
         {
-            n=n+NumberArr[i];
+            oddNumber = oddNumber + arrayString[i];
         }
     }
-    return n;
+    return oddNumber;
 }
 
-int [] array =new int [8];
-ArrayRandom (array);
-EvenNumb (array);
+int length = Input ("введите количество элементов массива: ");
+int [] array = new int [length];
+Console.Write($"массив: [ " + ArrayRandom(array) + "]");
+Console.WriteLine();
 Console.WriteLine($"сумма элементов на нечётных позициях = "+ EvenNumb (array));
