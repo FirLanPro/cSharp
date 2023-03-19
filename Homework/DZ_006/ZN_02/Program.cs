bab24f3e706aeb3d002fd0eecff4a7a0ddcc2093
FirLanPro <FirLanPro@yandex.ru>
2023-03-19 16:46:56 +0300

    DZ_006 ZN_02

--- Homework/DZ_006/ZN_02/Program.cs
+++ Homework/DZ_006/ZN_02/Program.cs
@@ -16,7 +16,7 @@ double Equation (double b1,double k1,double b2 ,double k2)
         double y;
         x =(b2-b1)/(k1-k2);
         y=k1*x+b1;
-        Console.WriteLine($"координаты точки пересечения: ({x} , {y})");
+        Console.WriteLine($"координаты точки пересечения: ({x} ; {y})");
     }
     return 0;
 }
