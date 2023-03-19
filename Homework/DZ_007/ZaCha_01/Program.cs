f682d0cb0e6560fd829e703b7e4e81b8f596ad5a
FirLanPro <FirLanPro@yandex.ru>
2023-03-19 17:29:10 +0300

    DZ_007 ZaCha_01

---
+++ Homework/DZ_007/ZaCha_01/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте двумерный массив размером m×n, 
+//заполненный случайными вещественными числами.
+
+int Input (string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double [,] ArrayRandom(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().NextDouble() + new Random().Next(-100,999);
+        }
+        
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ;  ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void InputOutput()
+{
+    int m=Input("количество строк: ");
+    int n=Input("количество столбцов: ");
+    double [,] array = new double [m,n];
+    Console.WriteLine();
+    ArrayRandom(array);
+    PrintArray(array);
+}
+
+InputOutput();
