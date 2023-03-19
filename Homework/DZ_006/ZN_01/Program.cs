da8148140d93fc0bf3e93f5141d2f7acbb40d80b
FirLanPro <FirLanPro@yandex.ru>
2023-03-19 07:20:34 +0300

    DZ_006 ZN_01

---
+++ Homework/DZ_006/ZN_01/Program.cs
@@ -0,0 +1,39 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int Input (string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void ArrayInput( int [] array)
+{
+    int size = array.Length;
+    for (int i=0; i<size; i++)
+    {
+        Console.Write($"введите число {i+1} :");
+        int number = Convert.ToInt32(Console.ReadLine());
+        array[i]=number;
+    }
+    Console.WriteLine();
+}
+
+int AboveZero (int [] array)
+ {
+    int moreZero=0;
+    int size = array.Length;
+    for (int i=0; i<size; i++)
+    {
+        if (array[i]>0)
+        {
+            moreZero=moreZero+1;
+        }
+    }
+    return moreZero;
+}
+
+int length = Input("Количество чисел: ");
+Console.WriteLine();
+int [] array =new int[length];
+ArrayInput(array);
+Console.WriteLine($"больше нуля : {AboveZero(array)} числа");    
