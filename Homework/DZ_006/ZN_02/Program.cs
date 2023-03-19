4c35941edd8d60b3c3abebc340f0fb68f44317af
FirLanPro <FirLanPro@yandex.ru>
2023-03-19 10:01:01 +0300

    DZ_006 ZN_02

--- Homework/DZ_006/ZN_02/Program.cs
+++ Homework/DZ_006/ZN_02/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+double Input (string text)
+{
+    Console.Write(text);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double Equation (double b1,double k1,double b2 ,double k2)
+{
+    if (k1!=k2)
+    {
+        double x;
+        double y;
+        x =(b2-b1)/(k1-k2);
+        y=k1*x+b1;
+        Console.WriteLine($"координаты точки пересечения: ({x} , {y})");
+    }
+    
+    return 0;
+}
+void InputResult (double k1,double k2)
+{
+    
+    if (k1==k2)
+    {
+        Console.WriteLine ("прямые не пересекаются");
+    }
+   
+}
+
+double k1 =Input("значение коэффициета k1: ");
+double b1 =Input("значение b1: ");
+double k2 =Input("значение коэффициета k2: ");
+double b2 =Input("значение b2: ");
+
+Equation ( b1,  k1,  b2,k2);
+Console.WriteLine();
+InputResult(k1, k2);
