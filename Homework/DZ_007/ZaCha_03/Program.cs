817514c69c1b93e53a67d00b7f23a302538a81c6
FirLanPro <FirLanPro@yandex.ru>
2023-03-20 14:20:26 +0300

    DZ_007 ZaCha_03

---
+++ Homework/DZ_007/ZaCha_03/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+int Input (string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int [,] ArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ;  ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double Summa (int [,]array, int m)
+{ 
+    double sum = 0;
+    double average=0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum =sum+array[i,j];
+        }
+        average = sum/m;
+        sum =0;
+        Console.WriteLine($"среднее арифметическое столбца {j+1} = {average} ; ");
+    }
+    return average;
+}
+
+void InputOutput()
+{
+    int m=Input("количество строк: ");
+    int n=Input("количество столбцов: ");
+    int [,] array = new int [m,n];
+    Console.WriteLine();
+    ArrayRandom(array);
+    PrintArray(array);
+    Summa(array,m);
+}
+
+InputOutput();
