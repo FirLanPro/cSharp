39336d2f12d24cda696c8ca73a092351f49a938b
FirLanPro <FirLanPro@yandex.ru>
2023-03-22 06:37:04 +0300

    L_07 Exm_01

---
+++ Lecture/L_07/Exm_01/Program.cs
@@ -0,0 +1,21 @@
+﻿// Рекурсия 
+// Собрать строку с числами от a до b, a<=b. вывод от меньшего к большему.
+
+string NumberFor(int a, int b) // итеративный метод
+{
+    string result = String.Empty;
+    for (int i =a; i <= b; i++) // от большего к меньшему. заменить инкремент на декримент
+    {
+        result += $"{i}";
+    }
+    return result;
+}
+
+string NumberRec(int a, int b) // рекурсия
+{
+    if (a <=b) return $"{a}" + NumberRec(a+1, b);// поменять местами в сборке строки. а и NumberRec
+    else return String.Empty;
+}
+
+Console.WriteLine (NumberFor(1, 10));
+Console.WriteLine(NumberRec(1, 10));
