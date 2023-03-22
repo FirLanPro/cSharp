76a12aef82cac45da8b92ee5e3cb765064f47e25
FirLanPro <FirLanPro@yandex.ru>
2023-03-22 05:19:40 +0300

    L_06 E_02

---
+++ Lecture/L_06/E_02/Program.cs
@@ -0,0 +1,13 @@
+﻿// в случайных точках консоли вывести случайные числа
+Random r = new Random();
+
+Console.CursorVisible = false;
+while (true)
+{
+    Console.SetCursorPosition(
+        left: r.Next(Console.WindowWidth),
+        top: r.Next(Console.WindowHeigth)  //Console не содержит определения WindowHeigth
+    );
+    Console.Write(r.Next(10));
+    Thread.Sleep(1000);
+}
