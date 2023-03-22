9be016ec14cf76ac11c495a03bf8f2fc5d47becb
FirLanPro <FirLanPro@yandex.ru>
2023-03-22 05:08:43 +0300

    L_06 E_01

---
+++ Lecture/L_06/E_01/Program.cs
@@ -0,0 +1,12 @@
+﻿// текст-экран
+string caption = "текст лвыалывалвлао";
+int screenWidthPosition = (Console.WindowWidth - caption.Length) / 2;
+int screenHeightPosition = Console.WindowHeight / 2;
+
+DrawText(caption, screenWidthPosition, screenHeightPosition);//DrawText не существует-  ?
+
+DrawText(
+    text: caption,
+    left: screenWidthPosition,
+    top: screenHeightPosition
+);
