3e37eba65d0f65859ec0af0e3ff325c750c4c454
FirLanPro <FirLanPro@yandex.ru>
2023-03-27 09:42:21 +0300

    DZ_008 ZCH_04

--- Homework/DZ_008/ZCH_04/Program.cs
+++ Homework/DZ_008/ZCH_04/Program.cs
@@ -10,19 +10,41 @@ int Input (string text)
 }
 int [,,] ArrayRandom(int[,,] array)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    int [] paddingLength = new int [array.GetLength(0) * array.GetLength(1) * array.GetLength(2)];
+    int  number;
+    for (int i = 0; i < paddingLength.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        paddingLength [i] = new Random().Next(10, 100);
+        number = paddingLength [i];
+        if (i >= 1)
         {
-            for (int k = 0; k < array.GetLength(2); k++)
+            for (int j = 0; j < i; j++)
             {
-             array[i, j, k] = new Random().Next(10,100); //не повторение чисел?
-             
+                while (paddingLength [i] == paddingLength [j])
+                {
+                    paddingLength [i] = new Random().Next(10, 100);
+                    j = 0;
+                    number = paddingLength [i];
+                }
+                number = paddingLength [i];
+            }
+        }
+    }
+    int count = 0; 
+    for (int x = 0; x < array.GetLength(0); x++)
+    {
+        for (int y = 0; y < array.GetLength(1); y++)
+        {
+            for (int z = 0; z < array.GetLength(2); z++)
+            {
+                array[x, y, z] = paddingLength [count];
+                count++;
             }
         }
     }
     return array;
 }
+
 void PrintArray(int [,,] array)
 {
         for (int k = 0; k < array.GetLength(2); k++)
@@ -41,13 +63,14 @@ void PrintArray(int [,,] array)
         }
         Console.WriteLine();
 }
+
 void Output ()
 {
     int x=Input("количество строк x: ");
-int y=Input("количество столбцов y: ");
-int z=Input("количество z: ");
-int [,,] array = new int [x,y,z];
-ArrayRandom(array);
-PrintArray(array);
+    int y=Input("количество столбцов y: ");
+    int z=Input("количество z: ");
+    int [,,] array = new int [x,y,z];
+    ArrayRandom(array);
+    PrintArray(array);
 }
 Output ();
