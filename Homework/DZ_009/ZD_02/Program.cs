170ae63a2cef3148161877d93906e0510e2860e3
FirLanPro <FirLanPro@yandex.ru>
2023-03-27 14:08:49 +0300

    DZ_009 ZD_02

--- Homework/DZ_009/ZD_02/Program.cs
+++ Homework/DZ_009/ZD_02/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. 
+// Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int Input (string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int AmountInInterval(int m, int n)
+{
+    int result = 0;
+    for(int i = m; i <n; i++) result +=i; //m==n ?
+    return result;
+}
+int M = Input("число от: ");
+int N = Input("число до: ");
+Console.WriteLine (AmountInInterval(M,N));
