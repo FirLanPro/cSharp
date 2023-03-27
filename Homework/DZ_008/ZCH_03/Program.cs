62f5f6f5ea0a6b960ac41dfabde67b76ef02e3f3
FirLanPro <FirLanPro@yandex.ru>
2023-03-27 04:06:30 +0300

    DZ_008 ZCH_03

--- Homework/DZ_008/ZCH_03/Program.cs
+++ Homework/DZ_008/ZCH_03/Program.cs
@@ -5,60 +5,103 @@ int Input (string text)
     return Convert.ToInt32(Console.ReadLine());
 }
 
-int [,] ArrayRandom(int[,] array)
+int [,] ArrayRandomOne(int[,] arrOne)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < arrOne.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < arrOne.GetLength(1); j++)
         {
-            array[i, j] = new Random().Next(0, 10);
+            arrOne[i, j] = new Random().Next(1, 5);
         }
     }
-    return array;
+    return arrOne;
 }
-int [,] ArrayRando( int [,]mas)
+int [,] ArrayRandomTwo( int [,]arrTwo)
 {
-    for (int i = 0; i < mas.GetLength(0); i++)
+    for (int i = 0; i < arrTwo.GetLength(0); i++)
     {
-        for (int j = 0; j < mas.GetLength(1); j++)
+        for (int j = 0; j < arrTwo.GetLength(1); j++)
         {
-            mas[i, j] = new Random().Next(0, 10);
+            arrTwo[i, j] = new Random().Next(1, 5);
         }
     }
-    return mas;
+    return arrTwo;
 }
-void PrintArray(int [,] array)
+int [,] ProductOfTwo( int [,]arrTwo, int [,]arrOne, int [,]oneTwo)
+
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+   
+    for (int i = 0; i < arrOne.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < arrTwo.GetLength(1); j++)
         {
-            Console.Write($"{array[i,j]} ;  ");
+            for (int k = 0;k < arrTwo.GetLength(0);k++ )
+            { 
+            oneTwo[i, j]= arrOne[i,k]*arrTwo[k,j]+ oneTwo[i,j]; // ??ik kj || kj ik правило умножения см.
+            Console.WriteLine(oneTwo[i,j]);
+            }
+        }
+    }
+    return oneTwo;
+}
+void PrintOneArray(int [,] arrOne)
+{
+    for (int i = 0; i < arrOne.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrOne.GetLength(1); j++)
+        {
+            Console.Write($"{arrOne[i,j]} ;  ");
         }
         Console.WriteLine();
     }
     Console.WriteLine();
 }
-void PrintArra( int [,]mas)
+void PrintTwoArray( int [,]arrTwo)
 {
-    for (int i = 0; i < mas.GetLength(0); i++)
+    for (int i = 0; i < arrTwo.GetLength(0); i++)
     {
-        for (int j = 0; j < mas.GetLength(1); j++)
+        for (int j = 0; j < arrTwo.GetLength(1); j++)
         {
-            Console.Write($"{mas[i,j]} ;  ");
+            Console.Write($"{arrTwo[i,j]} ;  ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+void PrintProductOfTwo( int [,]oneTwo)
+{
+    for (int i = 0; i < oneTwo.GetLength(0); i++)
+    {
+        for (int j = 0; j < oneTwo.GetLength(1); j++)
+        {
+            Console.Write($"{oneTwo[i,j]}    ;  ");
         }
         Console.WriteLine();
     }
     Console.WriteLine();
 }
 
-int m=Input("количество строк: ");
-int n=Input("количество столбцов: ");
-int [,] array = new int [m,n];
-int k=Input("количество строк: ");
-int l=Input("количество столбцов: ");
-int [,] mas = new int [k,l];
-ArrayRandom(array);
-PrintArray(array);
-ArrayRando(mas);
-PrintArra(mas);
+
+void Output ()
+{
+    int m=Input("количество строк первой матрицы: ");
+int n=Input("количество столбцов первой матрицы: ");
+int [,] arrOne = new int [m,n];
+int s=Input("количество строк второй матрицы: ");
+int l=Input("количество столбцов второй матрицы: ");
+int [,] arrTwo = new int [s,l];
+
+int [,] oneTwo = new int [arrOne.GetLength(0),arrTwo.GetLength(1)];
+    if (arrOne.GetLength(1) != arrTwo.GetLength(0))
+    {
+        Console.WriteLine($"Матрицы не перемножить");
+    }
+    
+    ArrayRandomOne(arrOne);
+PrintOneArray(arrOne);
+ArrayRandomTwo(arrTwo);
+PrintTwoArray(arrTwo);
+ProductOfTwo(arrOne,arrTwo,oneTwo);
+PrintProductOfTwo(oneTwo);
+}
+Output();
