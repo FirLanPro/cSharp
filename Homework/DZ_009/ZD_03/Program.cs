1759a0b6fcb53184c77fe8397495d6addd3a32f9
FirLanPro <FirLanPro@yandex.ru>
2023-03-27 15:11:26 +0300

    DZ_009 ZD_03

---
+++ Homework/DZ_009/ZD_03/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу 
+// вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n
+
+int Input (string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int A(int m, int n) 
+{
+    if (m==0) return n+1;
+    else if(m>0 && n==0) return A(m-1,1); 
+    else return A(m-1,A(m,n-1)); // m=4,n=1 тормозит
+}
+
+void InputOutput ()
+{
+    int m=Input("Введите число: ");
+    int n=Input("Введите число: ");
+    Console.WriteLine($"{A(m, n)}");
+}
+InputOutput ();
