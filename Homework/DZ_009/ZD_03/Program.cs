fac49df48a20fce506c289910bb994cdcce43d9f
FirLanPro <FirLanPro@yandex.ru>
2023-03-29 23:50:02 +0300

    DZ_009 ZD_03

--- Homework/DZ_009/ZD_03/Program.cs
+++ Homework/DZ_009/ZD_03/Program.cs
@@ -7,17 +7,21 @@ int Input (string text)
     Console.Write(text);
     return Convert.ToInt32(Console.ReadLine());
 }
-int A(int m, int n) // отрицательные числа?
+int A(int m, int n)
 {
     if (m==0) return n+1;
     else if(m>0 && n==0) return A(m-1,1); 
-    else return A(m-1,A(m,n-1)); // m=4,n=1 тормозит
+    else return A(m-1,A(m,n-1)); // при m=4,n=1 выводит значение, примерно, через 30 секунд.
 }
 
 void InputOutput ()
 {
     int m=Input("Введите число: ");
     int n=Input("Введите число: ");
-    Console.WriteLine($"{A(m, n)}");
+    if (m>=0 && n>=0)
+    {
+         Console.WriteLine($"{A(m, n)}");
+    }
+    else Console.WriteLine("по условию, два неотрицательных числа");
 }
 InputOutput ();
