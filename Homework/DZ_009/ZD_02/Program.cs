88f0ccf08f42a6c4a0615f0be99069ed6c6a1378
FirLanPro <FirLanPro@yandex.ru>
2023-03-30 13:45:07 +0300

    DZ_009 ZD_02

--- Homework/DZ_009/ZD_02/Program.cs
+++ Homework/DZ_009/ZD_02/Program.cs
@@ -9,10 +9,18 @@ int Input (string text)
 }
 int AmountInInterval(int m, int n)
 {
-    int result = 0;
-    for(int i = m; i <=n; i++) result +=i;
-    return result;
+    if ( m>0 && m<=n)  return m+ AmountInInterval(m+1,n) ;
+    else if ( m<=0 && m<=n) return AmountInInterval(m+1,n); // если m не натуральный элемент проверяем следующее элемент
+    else return 0;
 }
-int M = Input("число от: ");
-int N = Input("число до: ");
-Console.WriteLine (AmountInInterval(M,N));
+void InputOutput ()
+{
+    int M = Input("число от: ");
+    int N = Input("число до: ");
+    if ( N>0 ) // если N больше нуля, т.е. является натуральным, рекурсия выводит сумму до N. или само число N, при N=1
+    {
+        Console.WriteLine (AmountInInterval(M,N));
+    }
+    else Console.WriteLine ("натуральных элементов нет, промежуток полностью из ненатуральных чисел");
+}
+InputOutput ();
