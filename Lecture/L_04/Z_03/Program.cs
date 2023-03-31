d2ac6d1a469d548ad5d283e622508bae70949313
FirLanPro <FirLanPro@yandex.ru>
2023-03-31 04:37:24 +0300

    L_04 Z_03

---
+++ Lecture/L_04/Z_03/Program.cs
@@ -0,0 +1,10 @@
+﻿// вывод значений от 1 до 18.Факториал.
+double Factorial(int n)
+{
+    if(n==1) return 1;
+    else return n*Factorial(n-1);
+}
+for (int i=1;i<19;i++)
+{
+Console.WriteLine($"{i} ={Factorial(i)}");
+}
