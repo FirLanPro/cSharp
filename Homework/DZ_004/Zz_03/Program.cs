cd8d0e11ab60b863228aee73d590824d680f8f03
FirLanPro <FirLanPro@yandex.ru>
2023-04-03 11:42:19 +0300

    редактирование DZ_004 Zz_03

--- Homework/DZ_004/Zz_03/Program.cs
+++ Homework/DZ_004/Zz_03/Program.cs
@@ -1,9 +1,10 @@
-﻿// программу, которая задаёт массив из 8 элементов и выводит их на экран.
+﻿// программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
 
 
 void ArrayRandom (int [] NumberArr)
 {
-    int size=NumberArr.Length;
+    int size = NumberArr.Length;
     for (int i=0; i<size; i++)
     {
         NumberArr [i]= new Random().Next(1,50);
