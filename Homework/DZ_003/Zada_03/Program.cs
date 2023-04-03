61eb63f0c8ac8c633f296499096f164e76fbb253
FirLanPro <FirLanPro@yandex.ru>
2023-04-03 11:30:29 +0300

    редактирование DZ_003 Zada_03

--- Homework/DZ_003/Zada_03/Program.cs
+++ Homework/DZ_003/Zada_03/Program.cs
@@ -1,20 +1,20 @@
 ﻿// метод(-ы), который принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-int vvod (string text)
+int Input (string text)
 {
     Console.WriteLine(text);
     return Convert.ToInt32(Console.ReadLine());
 }
 
-string kub (int n)
+string Cube (int n)
 {
     for(int i=1;i<=n;i++)
     {
         Console.WriteLine($"число: {i}  | число в кубе: {i*i*i}");
         Console.WriteLine ($"-------------------------------");
     }
-Console.WriteLine();
-return "значения";
+    Console.WriteLine();
+    return "значения";
 }
 
-int n = vvod("Введите число до которого вычисляется куб: ");
-kub (n);
+int n = Input("Введите число до которого вычисляется куб: ");
+Cube (n);
