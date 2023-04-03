59a8367e21ebaa52737dd1acacb61e25cd331bf1
FirLanPro <FirLanPro@yandex.ru>
2023-04-03 10:59:43 +0300

    редактирование DZ_001 zadacha_001

--- Homework/DZ_001/zadacha_001/Program.cs
+++ Homework/DZ_001/zadacha_001/Program.cs
@@ -1,22 +1,16 @@
-﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа 
+// и выдаёт, какое число большее, а какое меньшее.
 Console.Write("Введите число 1: ");
-int a = Convert.ToInt32(Console.ReadLine());
+int min = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число 2: ");
-int b = Convert.ToInt32(Console.ReadLine());
-int max = 0;
-int min = 0;
-if (a > b)
+int max = Convert.ToInt32(Console.ReadLine());
+int m = 0;
+if (min > max)
 {
-    max = a;
-    min = b;
+    m = min;
+    max = min;
+    min = m;
 }
-else
-{
-    max = b;
-    min = a;
-}
-
-
 Console.Write("БОЛЬШЕЕ= ");
 Console.WriteLine(max);
 Console.Write("МЕНЬШЕЕ= ");
