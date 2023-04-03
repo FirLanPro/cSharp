d2f5a302f6fc85934ae533d6c695eeac5fdf3dfd
FirLanPro <FirLanPro@yandex.ru>
2023-04-03 11:58:31 +0300

    редактирование DZ_008 ZCH_05

--- Homework/DZ_008/ZCH_05/Program.cs
+++ Homework/DZ_008/ZCH_05/Program.cs
@@ -4,47 +4,47 @@ int [,] array = new int [4,4];
 
 int [,] ArrayRandom(int[,] array)
 {
-    int n=1;
+    int n = 1;
     
     for (int i = 0; i < array.GetLength(0); i++) // укоротить !
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] =n;
-            n=n+1;
+            array[i, j] = n;
+            n = n + 1;
         }
         for (i = 1; i < array.GetLength(0); i++)
         {
             int j=array.GetLength(1)-1;
-            array[i, j] =n;
-            n=n+1;
+            array[i, j] = n;
+            n = n + 1;
         }
     }
     for (int j=array.GetLength(1)-2; j>=0;j--)
     {
         int i=array.GetLength(0)-1;
-        array[i, j] =n;
-        n=n+1;
+        array[i, j] = n;
+        n = n + 1;
     }
     for (int i=array.GetLength(0)-2; i>0; i--)
     {
-        int j=0;
-        array[i, j] =n;
-        n=n+1;
+        int j = 0;
+        array[i, j] = n;
+        n = n + 1;
     }
     for (int i = 1; i < array.GetLength(0)-2; i++)
     {
-         for (int j=1; j<array.GetLength(1)-1; j++)
+         for (int j = 1; j<array.GetLength(1)-1; j++)
         {
-            array[i, j] =n;
-            n=n+1;
+            array[i, j] = n;
+            n = n + 1;
         }
     }
     for (int j=array.GetLength(1)-2;j>0;j-- )
     {
-        int i=2;
-        array[i, j] =n;
-        n=n+1;
+        int i = 2;
+        array[i, j] = n;
+        n = n + 1;
 
     }
     return array;
