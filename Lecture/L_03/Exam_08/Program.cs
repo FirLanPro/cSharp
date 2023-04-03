dda912e8e299734a3bb95f808ffd0740c6a25722
FirLanPro <FirLanPro@yandex.ru>
2023-04-03 12:44:19 +0300

    редактирование L_03 Exam_08

--- Lecture/L_03/Exam_08/Program.cs
+++ Lecture/L_03/Exam_08/Program.cs
@@ -1,20 +1,21 @@
 ﻿// замена символов в тексте
-string text = "аолрфралораоаовлаоа";    ///аргумент не удается преобразовать
+string text = "аолрфр  алораоа овлаоа";    
           
-string Replace( string text, char oldValue, char newValue)
+string Replace( string text, char oldValue, char newValue) // Replace не объявлена
 {
     string result = String.Empty;
     int length =text.Length;
     for (int i=0; i< length; i++)
     {
-        if (text[i]=oldValue) result = result + $"{newValue}";
+        if (text[i]==oldValue) result = result + $"{newValue}";
         else result = result + $"{text[i]}";
     
     }
     return result;
 }
-string newText = Replace(text, " ", "|");
+
+string newText = text.Replace( " ", "|");
 Console.WriteLine(newText);
 Console.WriteLine();
-newText= Replace(newText, "о", "HH");
+newText= newText.Replace( "о", "HH");
 Console.WriteLine(newText);
